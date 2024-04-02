4757d2a12601c6391bdf92a52ae4d17c0ce06586
rahul4732saini <rahul4732saini@gmail.com>
2024-04-02 16:18:23 +1100

    added API endpoint URLs to constants.py

---
+++ common/constants.py
@@ -0,0 +1,15 @@
+r"""
+Constants Module for the pyweather package
+
+This module comprises all the constants to be used throughout
+the pyweather package. These constants are designed to assist
+the other functionalities present within the package.
+"""
+
+# API URLs
+WEATHER_API = "https://api.open-meteo.com/v1/forecast"
+HISTORIC_WEATHER_API = "https://archive-api.open-meteo.com/v1/archive"
+MARINE_API = "https://marine-api.open-meteo.com/v1/marine"
+AIR_QUALITY_API = "https://air-quality-api.open-meteo.com/v1/air-quality"
+GEOCODING_API = "https://geocoding-api.open-meteo.com/v1/search"
+ELEVATION_API = "https://api.open-meteo.com/v1/elevation"
