85561b8e439b722fcb369ff34f40ec68c1eea04b
rahul4732saini <rahul4732saini@gmail.com>
2024-04-05 14:30:35 +0530

    Added get_hourly_forecast function in tools.py
    get_hourly_forecast used for retrieving the hourly weather forecast
    data from the Open-Meteo weather API.

--- common/tools.py
+++ common/tools.py
@@ -8,6 +8,7 @@ the pyweather package.
 from typing import Any
 
 import requests
+import pandas as pd
 
 from . import constants
 from errors import RequestError
@@ -134,3 +135,69 @@ def get_current_forecast(
     # the 'current' key in the 'params' dictionary.
     # The value associated with this key is returned as the result.
     return data[params["current"]]
+
+
+def get_hourly_forecast(
+    session: requests.Session, api: str, params: dict[str, str | int]
+) -> pd.DataFrame:
+    r"""
+    Base function for retrieving the hourly forecast data from supplied API.
+
+    This function is intended for internal use within the package and may not be called
+    directly by its users. It is exposed publicly for use by other modules within the package.
+
+    Params:
+        - session (requests.Session): A requests session object for making the API requests.
+        - api (str): Absolute URL of the API.
+        - params (dict[str, str | int]): Necessary parameters for the API request including the
+        coordinates of the location, requested data type, etc.
+
+    Returns:
+        - pd.DataFrame: Returns a pandas DataFrame of hourly weather forecast data comprising of
+        two columns namely 'time' and 'forecast' where 'time' column indicates the time of the
+        forecast data in ISO 8601 format (YYYY-MM-DDTHH:MM) and 'forecast' columns contains the
+        hourly weather forecast data.
+
+    Raises:
+        - KeyError: If 'hourly' key is not found in the `params` dictionary.
+        - RequestError: If there's an error while making the HTTP request to retrieve
+        forecast data from the API.
+    """
+
+    if not params.get("latitude") or not params.get("longitude"):
+        raise KeyError(
+            "`latitude` and `longitude` keys not found in the params dictionary "
+            "indicating the coordinates of the location."
+        )
+
+    if not params.get("hourly"):
+        raise KeyError(
+            "`hourly` key not found in the params dictionary with the requested weather data type."
+        )
+
+    with session.get(api, params=params) as response:
+        results: dict[str, Any] = response.json()
+
+        if response.status_code != 200:
+            message: str = results.get("reason", "Unknown Error")
+
+            # Raises a custom RequestError if the response status code is not 200 (OK).
+            # The error message is extracted from the API response.
+            raise RequestError(response.status_code, message)
+
+        # The "hourly" key in the results dictionary holds all the hourly
+        # weather forecast data key-value pairs.
+        hourly_data: dict[str, Any] = results["hourly"]
+
+        # pandas DataFrame containing time and hourly weather forecast data.
+        # The object comprises of two columns namely 'time' and 'forecast'.
+        # 'forecast' column data is retrived from the key-value pair named after the
+        # requested data type (eg. temperature_2m, weather_code, etc.) in the hourly_data mapping.
+        dataframe = pd.DataFrame(
+            {
+                "time": hourly_data["time"],
+                "forecast": hourly_data[params["hourly"]],
+            }
+        )
+
+        return dataframe
