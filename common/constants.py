cf3ae35465dbd6eb6916b764abc310b161ff3de3
rahul4732saini <rahul4732saini@gmail.com>
2024-04-06 16:47:06 +0530

    Added weather codes related constants.

--- common/constants.py
+++ common/constants.py
@@ -6,6 +6,9 @@ the pyweather package. These constants are designed to assist
 the other functionalities present within the package.
 """
 
+import json
+from pathlib import Path
+
 # API endpoint URLs
 WEATHER_API = "https://api.open-meteo.com/v1/forecast"
 HISTORIC_WEATHER_API = "https://archive-api.open-meteo.com/v1/archive"
@@ -13,3 +16,6 @@ MARINE_API = "https://marine-api.open-meteo.com/v1/marine"
 AIR_QUALITY_API = "https://air-quality-api.open-meteo.com/v1/air-quality"
 GEOCODING_API = "https://geocoding-api.open-meteo.com/v1/search"
 ELEVATION_API = "https://api.open-meteo.com/v1/elevation"
+
+WEATHER_CODES_FILE = Path("weather_codes.json")
+WEATHER_CODES = json.load(WEATHER_CODES_FILE.open())
