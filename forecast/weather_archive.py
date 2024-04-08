12cc5e47a9b7587a71f57517543d76966e2a95f0
rahul4732saini <rahul4732saini@gmail.com>
2024-04-08 16:02:21 +0530

    Added __init__ method and properties in Archive class
    Modified the super class __init__ method for including
    the start and end dates for the historical weather data
    and defined properties for the same. Defined the _resolve_date
    staticmethod for verifying and resolving date objects into strings.

--- forecast/weather_archive.py
+++ forecast/weather_archive.py
@@ -6,6 +6,8 @@ The Archive class allows users to extract vaious types of historical weather dat
 ranging from 1940 till the present.
 """
 
+from datetime import date, datetime
+
 import requests
 
 from common import constants
@@ -21,3 +23,52 @@ class Archive(BaseWeather):
 
     _session = requests.Session()
     _api = constants.WEATHER_HISTORY_API
+
+    def __init__(
+        self,
+        lat: int | float,
+        long: int | float,
+        start_date: str | date | datetime,
+        end_date: str | date | datetime,
+    ) -> None:
+        super().__init__(lat, long)
+
+        self.start_date = start_date
+        self.end_date = end_date
+
+    @staticmethod
+    def _resolve_date(target: str | date | datetime, var: str) -> date:
+        r"""
+        [PRIVATE] Verifies the supplied date argument, resolves it into the
+        string formatted date with the ISO-8601 format (YYYY-MM-DD).
+
+        The `var` parameter has to be the name of the actual date parameter
+        (`start_date` or `end_date`) for reference in custom error messages.
+        """
+
+        if isinstance(target, date | datetime):
+            target = date.strftime(r"%Y-%m-%d")
+
+        try:
+            datetime.strptime(target, r"%Y-%m-%d")
+
+        except (ValueError, TypeError):
+            raise ValueError(f"Invalid value for `{var}` parameter.")
+
+        return target
+
+    @property
+    def start_date(self) -> str:
+        return self._start_date
+
+    @start_date.setter
+    def start_date(self, __value: str | date | datetime) -> None:
+        self._start_date = self._resolve_date(__value, "start_date")
+
+    @property
+    def end_date(self) -> str:
+        return self._end_date
+
+    @end_date.setter
+    def end_date(self, __value: str | date | datetime) -> None:
+        self._end_date = self._resolve_date(__value, "end_date")
