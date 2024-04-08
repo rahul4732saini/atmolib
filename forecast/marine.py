b2615662b277b31610c50aeb22d0f8bac287fd6e
rahul4732saini <rahul4732saini@gmail.com>
2024-04-08 10:58:00 +0530

    Added MarineWeather class in marine.py

---
+++ forecast/marine.py
@@ -0,0 +1,17 @@
+r"""
+This module defines the MarineWeather class facilitating the retrieval of marine weather data from
+the Open-Meteo Marine Weather API based on latitudinal and longitudinal coordinates of the location.
+
+The MarineWeather class allows users to extract various types of marine weather information, including 
+current marine weather data and up to upcoming 8-days hourly and daily marine weather forecast data.
+"""
+
+from objects import BaseWeather
+
+
+class MarineWeather(BaseWeather):
+    r"""
+    MarineWeather class to extract marine weather data based on latitude and longitude coordinates.
+    It interacts with the Open-Meteo Marine Weather API to fetch the current or up to upcoming 8-days
+    hourly and daily marine weather forecast data.
+    """
