1ed5361436e787e81420a4537b79c71a25047ea1
rahul4732saini <rahul4732saini@gmail.com>
2024-04-08 11:18:39 +0530

    Populated __init__.py with imports and docs

---
+++ forecast/__init__.py
@@ -0,0 +1,21 @@
+r"""
+Weather Forecast Package
+------------------------
+
+This package facilitates the extraction of weather data including
+atmospheric, marine, and air quality data. It provides a comprehensive
+set of tools for weather data extraction.
+
+Classes Imported:
+- `Weather`: Extracts current or up to upcoming 16-days hourly and daily weather forecast data.
+- `MarineWeather`: Extracts current or up to upcoming 8-days hourly and daily marine weather forecast data.
+- `AirQuality`: Extracts current or up to upcoming 7-days hourly air quality forecast data.
+- `Archive`: Extracts historical weather data ranging from 1940 till the present.
+"""
+
+__all__ = "Weather", "Archive", "MarineWeather", "AirQuality"
+
+from .air import AirQuality
+from .weather import Weather
+from .marine import MarineWeather
+from .weather_archive import Archive
