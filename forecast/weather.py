d5d86d3e10e4120275de6dc8a8dcdd5c9b67626d
rahul4732saini <rahul4732saini@gmail.com>
2024-04-12 13:31:15 +0530

    Fixed inaccuracies in documentation

--- forecast/weather.py
+++ forecast/weather.py
@@ -15,7 +15,7 @@ from objects import BaseWeather
 class Weather(BaseWeather):
     r"""
     Weather class to extract weather data based on latitude and longitude coordinates.
-    It interacts with the Open-Meteo Weather API to fetch the current or upcoming 8-days
+    It interacts with the Open-Meteo Weather API to fetch the current or upcoming 16-days
     hourly and daily weather forecast data.
     """
 
@@ -34,18 +34,18 @@ class Weather(BaseWeather):
         at the supplied altitude in meters(m) from the ground level.
 
         Params:
-        - altitude (int): Altitude from the ground level. Must be in (2, 80, 120, 180).
-        - unit (str): Temperature unit. Must be 'celsius' or 'fahrenheit'.
+        - altitude (int): Altitude from the ground level, must be in 2, 80, 120 or 180.
+        - unit (str): Temperature unit, must be 'celsius' or 'fahrenheit'.
         """
 
         if altitude not in (2, 80, 120, 180):
             raise ValueError(
-                f"Expected `altitude` to be one of (2, 80, 120, 180). Got {altitude}"
+                f"Expected `altitude` to be 2, 80, 120 or 180, got {altitude}."
             )
 
         if unit not in ("celsius", "fahrenheit"):
             raise ValueError(
-                f"Expected `unit` to be one of 'celsius' or 'fahrenheit'. Got {unit!r}."
+                f"Expected `unit` to be 'celsius' or 'fahrenheit', got {unit!r}."
             )
 
         return self.get_current_weather_data(
@@ -78,8 +78,7 @@ class Weather(BaseWeather):
         Returns the current cloud cover in percentage(%) at the supplied level and coordinates.
 
         Params:
-        - level (str): Altitude level of the desired cloud coverage. Level supplied must be
-        one of the following:
+        - level (str): Altitude level of the desired cloud coverage, must be one of the following:
             - 'low' (clouds and fog up to an altitude of 3 km.)
             - 'mid' (clouds at an altitude between 3 km and 8 km.)
             - 'high' (clouds at an altitude higher than 8 km.)
@@ -87,7 +86,7 @@ class Weather(BaseWeather):
 
         if level not in ("low", "mid", "high"):
             raise ValueError(
-                f"Expected `level` to be one of ('low', 'mid', 'high'). Got {level!r}."
+                f"Expected `level` to be 'low', 'mid' or 'high', got {level!r}."
             )
 
         return self.get_current_weather_data({"current": f"cloud_cover_{level}"})
@@ -102,12 +101,12 @@ class Weather(BaseWeather):
         combining wind chill factor, relative humidity and solar radiation.
 
         Params:
-        - unit (str): Temperature unit. Must be 'celsius' or 'fahrenheit'.
+        - unit (str): Temperature unit must be 'celsius' or 'fahrenheit'.
         """
 
         if unit not in ("celsius", "fahrenheit"):
             raise ValueError(
-                f"Expected `unit` to be 'celsius' or 'fahrenheit'. Got {unit!r}."
+                f"Expected `unit` to be 'celsius' or 'fahrenheit', got {unit!r}."
             )
 
         return self.get_current_weather_data(
@@ -123,8 +122,8 @@ class Weather(BaseWeather):
         Returns the current wind speed at the supplied altitude and in the supplied unit.
 
         Params:
-        - altitude (int): Altitude from the ground level. Must be in (10, 80, 120, 180).
-        - unit (str): Wind speed unit. The unit must be one of the following:
+        - altitude (int): Altitude from the ground level, Must be 10, 80, 120 or 180.
+        - unit (str): Wind speed unit, must be one of the following:
             - 'kmh' (kilometers per hour)
             - 'mph' (miles per hour)
             - 'ms' (meter per second)
@@ -133,12 +132,12 @@ class Weather(BaseWeather):
 
         if altitude not in (2, 80, 120, 180):
             raise ValueError(
-                f"Expected `altitute` to be one of (10, 80, 120, 180). Got {altitude}"
+                f"Expected `altitute` to be 10, 80, 120 or 180, got {altitude}."
             )
 
         if unit not in ("kmh", "mph", "ms", "kn"):
             raise ValueError(
-                f"Expected `unit` to be one of ('kmh', 'mph', 'ms', 'kn'). Got {unit!r}."
+                f"Expected `unit` to be 'kmh', 'mph', 'ms' or 'kn', got {unit!r}."
             )
 
         return self.get_current_weather_data(
@@ -153,12 +152,12 @@ class Weather(BaseWeather):
         Returns the current wind direction at the supplied altitude and in the supplied unit.
 
         Params:
-        - altitude (int): Altitude from the ground level. Must be in (10, 80, 120, 180).
+        - altitude (int): Altitude from the ground level, must be 10, 80, 120 or 180.
         """
 
         if altitude not in (2, 80, 120, 180):
             raise ValueError(
-                f"Expected `altitude` to be one of (10, 80, 120, 180). Got {altitude}."
+                f"Expected `altitude` to be 10, 80, 120 or 180, got {altitude}."
             )
 
         return self.get_current_weather_data({"current": f"wind_direction_{altitude}m"})
@@ -170,7 +169,7 @@ class Weather(BaseWeather):
         Returns the current wind gusts above 10 meters(m) from ground level in the supplied unit.
 
         Params:
-        - unit (str): Wind speed unit. The unit must be one of the following:
+        - unit (str): Wind speed unit, must be one of the following:
             - 'kmh' (kilometers per hour)
             - 'mph' (miles per hour)
             - 'ms' (meter per second)
@@ -179,7 +178,7 @@ class Weather(BaseWeather):
 
         if unit not in ("kmh", "mph", "ms", "kn"):
             raise ValueError(
-                f"Expected `unit` to be one of ('kmh', 'mph', 'ms', 'kn'). Got {unit!r}."
+                f"Expected `unit` to be 'kmh', 'mph', 'ms' or 'kn', got {unit!r}."
             )
 
         return self.get_current_weather_data(
@@ -201,13 +200,11 @@ class Weather(BaseWeather):
         the supplied coordinates.
 
         Params:
-        - unit: Precipitation unit. It must be in ('mm', 'inch').
+        - unit: Precipitation unit, must be 'mm' or 'inch'.
         """
 
         if unit not in ("mm", "inch"):
-            raise ValueError(
-                f"Expected `unit` to be one of 'mm' or 'inch'. Got {unit!r}."
-            )
+            raise ValueError(f"Expected `unit` to be 'mm' or 'inch'. Got {unit!r}.")
 
         return self.get_current_weather_data(
             {"current": "precipitation", "precipitation_unit": unit}
