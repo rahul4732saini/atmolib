b404f6562431d493e7a74504ff21f9bbd581317e
rahul4732saini <rahul4732saini@gmail.com>
2024-04-17 22:18:26 +0530

    Renamed constant WEATHER_HISTORY_API -> WEATHER_ARCHIVE_API
    Renamed the constant for better reference to its purpose.

--- common/constants.py
+++ common/constants.py
@@ -12,7 +12,7 @@ from typing import Literal
 
 # API endpoint URLs.
 WEATHER_API = "https://api.open-meteo.com/v1/forecast"
-WEATHER_HISTORY_API = "https://archive-api.open-meteo.com/v1/archive"
+WEATHER_ARCHIVE_API = "https://archive-api.open-meteo.com/v1/archive"
 MARINE_API = "https://marine-api.open-meteo.com/v1/marine"
 AIR_QUALITY_API = "https://air-quality-api.open-meteo.com/v1/air-quality"
 GEOCODING_API = "https://geocoding-api.open-meteo.com/v1/search"
