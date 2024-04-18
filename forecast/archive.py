e3999049bee8f926137e429c4c689fdbd53b4bff
rahul4732saini <rahul4732saini@gmail.com>
2024-04-18 17:11:54 +0530

    Optimized Archive methods with pre-defined methods
    Optimized the methods in Archive class with pre-defined methods
    in the BaseWeather class removing boilerplate code.

--- forecast/archive.py
+++ forecast/archive.py
@@ -143,10 +143,7 @@ class WeatherArchive(BaseWeather, BaseMeteor):
         if altitude not in (10, 100):
             raise ValueError(f"Expected `altitude` to be 10 or 100, got {altitude}.")
 
-        if unit not in ("kmh", "mph", "ms", "kn"):
-            raise ValueError(
-                f"Expected `unit` to be 'kmh', 'mph', 'ms' or 'kn', got {unit!r}."
-            )
+        self._verify_wind_speed_unit(unit)
 
         return self._get_periodical_data(
             {"hourly": f"wind_speed_{altitude}m", "wind_speed_unit": unit}
@@ -184,11 +181,7 @@ class WeatherArchive(BaseWeather, BaseMeteor):
             - 'ms' (meter per second)
             - 'kn' (knots)
         """
-
-        if unit not in ("kmh", "mph", "ms", "kn"):
-            raise ValueError(
-                f"Expected `unit` to be 'kmh', 'mph', 'ms' or 'kn', got {unit!r}."
-            )
+        self._verify_wind_speed_unit(unit)
 
         return self._get_periodical_data(
             {"hourly": f"wind_gusts_10m", "wind_speed_unit": unit}
