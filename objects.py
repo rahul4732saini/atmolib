172e50923277679670ce14a3ae8d87fd9d7e6852
rahul4732saini <rahul4732saini@gmail.com>
2024-04-18 10:16:06 +0530

    Added BaseWeather.get_daily_dominant_wind_direction method

--- objects.py
+++ objects.py
@@ -246,3 +246,10 @@ class BaseWeather(BaseMeteor):
         return self._get_periodical_data(
             {"daily": f"apparent_temperature_{type_}", "temperature_unit": unit}
         )
+
+    def get_daily_dominant_wind_direction(self) -> pd.DataFrame:
+        r"""
+        Returns a pandas DataFrame of daily dominant wind direction in degrees data 10 meters(m)
+        above the ground level at the specified coordinates.
+        """
+        return self._get_periodical_data({"daily": "wind_direction_10m_dominant"})
