c077115eaedf1c0c899c52db812afa9d53a268b4
rahul4732saini <rahul4732saini@gmail.com>
2024-04-19 17:57:11 +0530

    Added file documentation in __init__.py file

--- __init__.py
+++ __init__.py
@@ -1,6 +1,21 @@
 r"""
 Pyweather Package
 -----------------
+
+PyWeather is a Python library that provides easy and fast access to meteorology data through
+the Open-Meteo APIs. It enables developers to extract weather, historical weather, air quality,
+and marine weather data effortlessly, empowering them to integrate accurate meteorological
+information seamlessly into their applications.
+
+Features:
+- Weather data extraction: Retrieve current and forecast weather data for any location.
+- Historical weather data: Access past weather data for analysis and reporting.
+- Air quality information: Obtain real-time air quality data to monitor environmental conditions.
+- Marine weather data: Retrieve marine weather forecasts for maritime activities.
+
+Author: rahul4732saini
+Version: ---
+License: MIT License, see LICENSE for more details. (https://opensource.org/licenses/MIT)
 """
 
 __all__ = (
