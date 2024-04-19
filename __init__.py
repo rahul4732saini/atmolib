fa2df812e4e67b167971c3b9b9f5310340206416
rahul4732saini <rahul4732saini@gmail.com>
2024-04-19 17:58:41 +0530

    Modified method documentations

--- __init__.py
+++ __init__.py
@@ -38,17 +38,17 @@ def get_elevation(lat: int | float, long: int | float) -> float:
     Retrieves elevation data from Open-meteo elevation
     API based on the latitude and longitude coordinates.
 
-    Params:
+    #### Params:
         - lat (int | float): latitudinal coordinates of the location.
         - long (int | float): longitudinal coordinates of the location.
 
-    Raises:
+    #### Raises:
         - ValueError: If `lat` or `long` are not integers or floating point numbers.
 
-    Returns:
+    #### Returns:
         - float: Returns the elevation at the supplied coordinates in meters(m).
 
-    Example:
+    #### Example:
         >>> altitude = get_elevation(26.91, 32.89)
         >>> print(altitude)
         300.0  # Example elevation value in meters
@@ -71,16 +71,16 @@ def get_city_details(name: str, count: int = 5) -> list[dict[str, Any]] | None:
     Retrieves the city details from Open-meteo
     geocoding API based on the name of the city.
 
-    Params:
+    #### Params:
         - name (str): The name of the city to retrieve details for.
         - count (int): The number of results to be shown.
 
-    Returns:
+    #### Returns:
         - List[Dict[str, Any]] | None: Returns a list of dictionaries containing details of the city.
         Each dictionary represents a result, containing various information about the city. None is
         returned if no cities corresponding to the supplied name are found in the database.
 
-    Raises:
+    #### Raises:
         - ValueError: If `count` is not a positive integer.
     """
 
