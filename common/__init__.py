e3cc5c21b22ded9b45489158c056d28075ffe171
rahul4732saini <rahul4732saini@gmail.com>
2024-04-19 21:49:13 +0530

    Added file documentation in common/__init__.py

--- common/__init__.py
+++ common/__init__.py
@@ -1,3 +1,11 @@
+r"""
+Common package
+--------------
+
+This packages comprises constants and utility functions assisting
+others functionalities throughout the pyweather package.
+"""
+
 __all__ = ["constants", "tools"]
 
 from . import constants
