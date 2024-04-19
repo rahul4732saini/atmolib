9663749f2ff2503854ed2a987344a7bbd265241a
rahul4732saini <rahul4732saini@gmail.com>
2024-04-19 21:20:33 +0530

    Fixed documentation and reformatted constants.py

--- common/constants.py
+++ common/constants.py
@@ -1,9 +1,9 @@
 r"""
-Constants Module for the pyweather package
+Constants Module
+----------------
 
-This module comprises all the constants used throughout the
-pyweather package. These constants are designed to assist
-other functionalities present within the package.
+This module comprises all the constants used throughout the pyweather package.
+These constants are designed to assist other functionalities present within the package.
 """
 
 import json
@@ -18,7 +18,7 @@ AIR_QUALITY_API = "https://air-quality-api.open-meteo.com/v1/air-quality"
 GEOCODING_API = "https://geocoding-api.open-meteo.com/v1/search"
 ELEVATION_API = "https://api.open-meteo.com/v1/elevation"
 
-# Weather code JSON file comprising weather codes mapped with corresponding
+# Weather codes JSON file comprising weather codes mapped with the corresponding
 # description of the same. `WEATHER_CODES` loads the JSON file into a dictionary.
 WEATHER_CODES_FILE = Path("weather_codes.json")
 WEATHER_CODES = json.load(WEATHER_CODES_FILE.open())
@@ -38,21 +38,31 @@ PRESSURE_LEVELS = Literal["sealevel", "surface"]
 # as request parameter for extracting data from the Open-Meteo Weather API.
 PRESSURE_LEVEL_MAPPING = {"sealevel": "pressure_msl", "surface": "surface_pressure"}
 
-# Holds the altitude in meters(m) above the surface level.
+# Holds the altitude in meters(m) above the surface level for different request types.
 TEMPERATURE_ALTITUDE = Literal[2, 80, 120, 180]
 WIND_ALTITUDE = Literal[10, 80, 120, 180]
+ARCHIVE_WIND_ALTITUDES = Literal[10, 100]
 
-# Available depth options in centimeters(cm)  for
-# historical soil temperature data extraction.
-ARCHIVE_SOIL_TEMP_DEPTH = {
+# Available depth-range options in centimeters(cm) for
+# historical soil temperature/moisture data extraction.
+ARCHIVE_SOIL_DEPTH = {
     range(7): "0_to_7",
     range(7, 28): "7_to_28",
     range(28, 100): "28_to_100",
     range(100, 256): "100_to_255",
 }
 
-# Available altitude options in meters(m) for historical wind data.
-ARCHIVE_WIND_ALTITUDES = Literal[10, 100]
+# Available depth options in centimeters(cm) for soil temperature data extraction.
+SOIL_TEMP_DEPTH = Literal[0, 6, 18, 54]
+
+# Available depth-range options in centimeters(cm) for soil moisture data extraction.
+SOIL_MOISTURE_DEPTH = {
+    range(1): "0_to_1",
+    range(1, 3): "1_to_3",
+    range(3, 9): "3_to_9",
+    range(9, 27): "9_to_27",
+    range(27, 81): "27_to_81",
+}
 
 # Air Quality Index sources.
 AQI_SOURCES = Literal["european", "us"]
