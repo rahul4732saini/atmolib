228204f92997d3931c03338e4488c06cd8ab2ff5
rahul4732saini <rahul4732saini@gmail.com>
2024-04-23 21:47:55 +0530

    Added new imports in __init__.py
    Imported errors.py and objects.py file in __init__.py

--- pyweather/__init__.py
+++ pyweather/__init__.py
@@ -27,8 +27,11 @@ __all__ = (
     "MarineWeather",
     "AirQuality",
     "constants",
+    "errors",
+    "objects",
 )
 
+from . import errors, objects
 from .common import tools, constants
 from .common.tools import get_city_details, get_elevation
 from .meteorology import Weather, WeatherArchive, AirQuality, MarineWeather
