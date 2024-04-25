04c0b8b93bbab47fcfcb1ae1f25c8159af521f15
rahul4732saini <rahul4732saini@gmail.com>
2024-04-25 15:45:51 +0530

    Added fixtures in tests/meteorology/conftest.py file.
    Defined fixtures `valid_marine_coordinates` and `invalid_marine_coordinates`

---
+++ tests/meteorology/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def valid_marine_coordinates() -> tuple[tuple[int, int]]:
+    return ((49.10, -39.55), (-8.30, 68.19), (-57.29, 122.78), (-44.62, -5.57))
+
+
+@pytest.fixture
+def invalid_marine_coordinates() -> tuple[tuple[int, int]]:
+    return ((26.91, 75.54), (68.46, 118.64), (-2.93, -61.77), (-24.89, 144.93))
