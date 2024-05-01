a692080822a0b41a78a9bb7ba92e9bf97f922167
rahul4732saini <rahul4732saini@gmail.com>
2024-05-01 15:19:06 +0530

    Update `atmolib/common/__init__.py`
    Removed redundant parenthesis from the `__all__`
    attribute in the `atmolib/common/__init__.py` file.

--- atmolib/common/__init__.py
+++ atmolib/common/__init__.py
@@ -6,7 +6,7 @@ This packages comprises constants and utility functions assisting
 others functionalities throughout the atmolib package.
 """
 
-__all__ = ["constants", "tools"]
+__all__ = "constants", "tools"
 
 from . import constants
 from . import tools
