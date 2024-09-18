2fd525385018d0ac648eef9f1faf4f1bd97229c9
rahul4732saini <rahul4732saini@gmail.com>
2024-09-18 22:36:05 +0530

    Fix documentation in __init__.py

--- atmolib/meteorology/__init__.py
+++ atmolib/meteorology/__init__.py
@@ -8,12 +8,9 @@ of classes and function designed for the same.
 
 ### Classes Exported:
     - `Weather`: Extracts current or upto upcoming 16-days hourly and daily weather forecase data.
-
-Classes Exported:
-- `Weather`: Extracts current or up to upcoming 16-days hourly and daily weather forecast data.
-- `MarineWeather`: Extracts current or up to upcoming 8-days hourly and daily marine weather forecast data.
-- `AirQuality`: Extracts current or up to upcoming 7-days hourly air quality forecast data.
-- `WeatherArchive`: Extracts historical weather data ranging from the year 1940 till the present.
+    - `MarineWeather`: Extracts current or upto upcomding 8-days hourly and daily marine weather forecast data.
+    - `AirQuality`: Extracts current or upto upcoming 7-days hourly air quality forecast data.
+    - `WeatherArchive`: Extracts historical weather data raning from the year 1940 till present.
 """
 
 __all__ = "Weather", "WeatherArchive", "MarineWeather", "AirQuality"
