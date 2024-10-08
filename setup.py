d16bf180ccc02da3b23e7b562bc2b1e9da4abb68
rahul4732saini <rahul4732saini@gmail.com>
2024-10-08 21:42:51 +0530

    Remove redundant code in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 # Extracts the long description from the README.md file.
-DESCRIPTION_FILE = Path("./README.md").absolute()
+DESCRIPTION_FILE = Path("README.md")
 DESCRIPTION = DESCRIPTION_FILE.read_text()
 
 # Extracts the version from the atmolib/version.py file.
-VERSION_FILE = Path("./atmolib/version.py").absolute()
+VERSION_FILE = Path("atmolib/version.py")
 VERSION = VERSION_FILE.read_text().strip().replace("version = ", "").replace('"', "")
 
 setup(
