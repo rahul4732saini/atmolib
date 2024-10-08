c212413c8f2a40904edb93dbb2ad7748a235edad
rahul4732saini <rahul4732saini@gmail.com>
2024-10-08 15:51:22 +0530

    Update file documentation in __init__.py

--- atmolib/__init__.py
+++ atmolib/__init__.py
@@ -1,17 +1,17 @@
 """
 Atmolib Package
------------------
+---------------
 
 Atmolib is a Python library that provides easy and fast access to meteorology data through
 Open-Meteo APIs. It enables developers to extract weather, historical weather, air quality,
 and marine weather data effortlessly, empowering them to integrate accurate meteorological
-information seamlessly into their applications.
+information seamlessly into their python code.
 
 #### Features:
 - Weather data extraction: Retrieve current and forecast weather data for any location.
 - Historical weather data: Access past weather data for analysis and reporting.
 - Air quality information: Obtain real-time air quality data to monitor environmental conditions.
-- Marine weather data: Retrieve marine weather forecasts for maritime activities.
+- Marine weather data: Retrieve currrent and forecast marine weather data for maritime activities.
 
 Author: rahul4732saini
 License: MIT, see LICENSE for more details. (https://opensource.org/licenses/MIT)
