93d86092c9d092f9459251bef27f519f84d356f0
rahul4732saini <rahul4732saini@gmail.com>
2024-10-08 15:32:03 +0530

    Update errors.py
    Updated the documentation in the `RequestError`
    class for better readability.

--- atmolib/errors.py
+++ atmolib/errors.py
@@ -11,11 +11,16 @@ class RequestError(Exception):
     """
     RequestError class for handling API request errors.
 
-    This exception is raised when there's an error during API requests,
-    encompassing various issues such as server-related errors
-    (eg.: HTTP status codes other than 200).
+    This exception is raised for all API endpoint requests returning
+    a status code which do not fall in the 2xx series encompassing a
+    failure in the request process.
     """
 
     def __init__(self, status_code: int, message: str | None = None) -> None:
+        """
+        Initializes the RequestError with a status
+        code and an optional descriptive message.
+        """
+
         message = f"Server responded with status code {status_code}. {message}"
         super().__init__(message)
