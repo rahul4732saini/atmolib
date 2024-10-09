cd9926abfd943272c06805c7a96efdf0ea2aacaa
rahul4732saini <rahul4732saini@gmail.com>
2024-10-09 21:59:12 +0530

    Minor formatting fixes in __init__.py

--- atmolib/meteorology/__init__.py
+++ atmolib/meteorology/__init__.py
@@ -2,20 +2,24 @@ r"""
 Meteorology Package
 -------------------
 
-This package facilitates the extraction of meteorological data, encompassing weather,
-historical weather, marine conditions, and air quality data. If offers a robust suite
-of classes and function designed for the same.
+This package facilitates the extraction of meteorological data, encompassing
+weather, historical weather, marine conditions, and air quality data. If offers
+a robust suite of classes and function designed for the same.
 
 ### Classes Exported:
-    - `Weather`: Extracts current or upto upcoming 16-days hourly and daily weather forecast data.
-    - `MarineWeather`: Extracts current or upto upcoming 8-days hourly and daily marine weather forecast data.
-    - `AirQuality`: Extracts current or upto upcoming 7-days hourly air quality forecast data.
-    - `WeatherArchive`: Extracts historical weather data ranging from the year 1940 till present.
+    - `Weather`: Extracts current or upto upcoming 16-days hourly and daily
+    weather forecast data.
+    - `MarineWeather`: Extracts current or upto upcoming 8-days hourly and
+    daily marine weather forecast data.
+    - `AirQuality`: Extracts current or upto upcoming 7-days hourly air
+    quality forecast data.
+    - `WeatherArchive`: Extracts historical weather data ranging from the
+    year 1940 till present.
 """
 
 __all__ = "Weather", "WeatherArchive", "MarineWeather", "AirQuality"
 
-from .air import AirQuality
 from .weather import Weather
-from .marine import MarineWeather
 from .archive import WeatherArchive
+from .air import AirQuality
+from .marine import MarineWeather
