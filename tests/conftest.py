d01f9009a415ff14bb99d79ec5e896d493b73bc6
rahul4732saini <rahul4732saini@gmail.com>
2024-10-14 10:18:26 +0530

    Refactor: remove redundant code in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,12 @@ import pytest
 
 @pytest.fixture
 def valid_coordinates() -> tuple[tuple[float, float], ...]:
-    return ((26.91, 75.54), (38.96, -112.66), (-67.47, 119.18), (-0.76, -21.46))
+    return (
+        (26.91, 75.54),
+        (38.96, -112.66),
+        (-67.47, 119.18),
+        (-0.76, -21.46),
+    )
 
 
 @pytest.fixture
@@ -13,6 +18,4 @@ def invalid_coordinates() -> tuple[tuple[float, float], ...]:
         (26.91, -180.1),
         (90.1, 154.44),
         (75.54, 180.1),
-        (90.1, 180.1),
-        (-90.1, -180.1),
     )
