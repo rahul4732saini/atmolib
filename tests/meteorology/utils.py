bebc3f6ac719b74a4d314104e628235f5afebc57
rahul4732saini <rahul4732saini@gmail.com>
2024-10-25 15:01:36 +0530

    Update utils.py
    Defined `_verify_positive_data_series` function for verifying that
    all the values stored within the specified pandas Series object are
    greater than 0.

--- tests/meteorology/utils.py
+++ tests/meteorology/utils.py
@@ -5,3 +5,15 @@ Utils.py
 This module comprises utility functions specifically designed
 to support test functions and methods defined within the project.
 """
+
+import pandas as pd
+
+
+def verify_positive_data_series(series: pd.Series) -> None:
+    """
+    Verifies that all the values stored within the
+    specified pandas Series object are greater than 0.
+    """
+
+    assert isinstance(series, pd.Series)
+    assert (series >= 0).all()
