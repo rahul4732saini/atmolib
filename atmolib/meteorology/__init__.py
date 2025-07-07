b1bd04cac2fb0aa6d98e69d9dd66e96a363bbea3
rahul4732saini <rahul4732saini@gmail.com>
2025-07-07 16:52:41 +0530

    Update file docs in meteorology/__init__.py

--- atmolib/meteorology/__init__.py
+++ atmolib/meteorology/__init__.py
@@ -7,14 +7,15 @@ weather, historical weather, marine conditions, and air quality data. If offers
 a robust suite of classes and function designed for the same.
 
 ### Classes Exported:
-    - `Weather`: Extracts current or upto upcoming 16-days hourly and daily
-    weather forecast data.
-    - `MarineWeather`: Extracts current or upto upcoming 8-days hourly and
-    daily marine weather forecast data.
-    - `AirQuality`: Extracts current or upto upcoming 7-days hourly air
-    quality forecast data.
-    - `WeatherArchive`: Extracts historical weather data ranging from the
-    year 1940 till present.
+
+- `Weather`: Extracts current or upto upcoming 16-days hourly and daily
+weather forecast data.
+- `MarineWeather`: Extracts current or upto upcoming 8-days hourly and
+daily marine weather forecast data.
+- `AirQuality`: Extracts current or upto upcoming 7-days hourly air
+quality forecast data.
+- `WeatherArchive`: Extracts historical weather data ranging from the
+year 1940 till present.
 """
 
 __all__ = "Weather", "WeatherArchive", "MarineWeather", "AirQuality"
