dc63e6a99148ffdab938d0e0030d8144ddeddc45
rahul4732saini <rahul4732saini@gmail.com>
2025-08-03 10:44:26 +0530

    Fix verifier function in utils.py
    Fixed the 'verify_temperature_data_series' to not verify if the
    temperature data is positive as it is meaningless, and it might
    also be negative.

--- tests/utils.py
+++ tests/utils.py
@@ -36,8 +36,6 @@ def verify_temperature_data_series(series: pd.Series) -> None:
     specified pandas Series object.
     """
 
-    verify_positive_data_series(series)
-
     assert isinstance(series, pd.Series)
     assert issubclass(series.dtype.type, np.integer | np.floating)
 
